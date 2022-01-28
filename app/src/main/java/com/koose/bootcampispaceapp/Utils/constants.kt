f381025602588c6228ad22c6cbba5b0235c1c7ee
Koose <KooseTheBlogger@gmail.com>
2022-01-28 14:48:37 +0000

    all names in CONST in Utils folder

--- app/src/main/java/com/koose/bootcampispaceapp/Utils/constants.kt
+++ app/src/main/java/com/koose/bootcampispaceapp/Utils/constants.kt
@@ -3,5 +3,12 @@ package com.koose.bootcampispaceapp.Utils
 import com.koose.bootcampispaceapp.R
 
 object constants {
-    val KOOSE = R.
+    val GODFRED = R.raw.godfred
+    val CLAUDIA = R.raw.claudia
+    val FATIMAH = R.raw.fatimah
+    val FEMI = R.raw.femi
+    val FEMI_TWO = R.raw.femi_two
+    val KWADWO = R.raw.kwadwo
+    val ZIBAH = R.raw.zibah
+    val ALBERT = R.raw.albert
 }
