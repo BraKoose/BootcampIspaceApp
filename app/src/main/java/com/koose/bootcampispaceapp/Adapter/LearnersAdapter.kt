6210d197f84e5fd689c6bae40fc152d7c308ff13
Koose <KooseTheBlogger@gmail.com>
2022-01-28 15:17:55 +0000

    recycler view created and implemented members

---
+++ app/src/main/java/com/koose/bootcampispaceapp/Adapter/LearnersAdapter.kt
@@ -0,0 +1,31 @@
+package com.koose.bootcampispaceapp.Adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class LearnersAdapter:
+    RecyclerView.Adapter<LearnersAdapter.LeanersViewHolder>() {
+
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeanersViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: LeanersViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return
+    }
+
+    class LeanersViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+
+}
--- app/src/main/java/com/koose/bootcampispaceapp/MainActivity.kt
+++ app/src/main/java/com/koose/bootcampispaceapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.koose.bootcampispaceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.koose.bootcampispaceapp.data.learnersData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
