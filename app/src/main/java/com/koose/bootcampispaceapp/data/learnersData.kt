81f4a1225299ab00ad004cf3367a5f70e79cbcf8
Koose <KooseTheBlogger@gmail.com>
2022-01-28 14:09:10 +0000

    data class on learners

---
+++ app/src/main/java/com/koose/bootcampispaceapp/data/learnersData.kt
@@ -0,0 +1,11 @@
+package com.koose.bootcampispaceapp.data
+
+data class learnersData(
+    val name: String,
+    val email: String,
+    val gender: String,
+    val profession: String,
+    val phone: String,
+    val location: String,
+    val image: Int
+    )
