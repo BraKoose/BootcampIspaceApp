da5cd05444bc9a62b0b580e5dea89ca059bc2617
Koose <KooseTheBlogger@gmail.com>
2022-01-28 15:53:29 +0000

    declaring recycler view on recyclerID in the XML

--- app/src/main/java/com/koose/bootcampispaceapp/MainActivity.kt
+++ app/src/main/java/com/koose/bootcampispaceapp/MainActivity.kt
@@ -2,13 +2,17 @@ package com.koose.bootcampispaceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.koose.bootcampispaceapp.data.learnersData
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recycler: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        recycler = findViewById(R.id.Recycler)
     }
 }
